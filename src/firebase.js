9560d37b10555829e692f114b7f39a2f8459a2dc
PragyaTomar19188 <85572016+PragyaTomar19188@users.noreply.github.com>
2021-09-26 21:49:07 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAzDRSuNvmNqkS_ocL6_qWEBeQ5i8zMYCc",
+    authDomain: "whatsapp-c36cc.firebaseapp.com",
+    projectId: "whatsapp-c36cc",
+    storageBucket: "whatsapp-c36cc.appspot.com",
+    messagingSenderId: "376571072540",
+    appId: "1:376571072540:web:0b533338b27b90441acfd9",
+    measurementId: "G-C1D35BWQ6S"
+  };
